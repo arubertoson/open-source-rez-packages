2439fd1f09c21b8eaeb280f00e1e0ae92f337b6b
macke <macke@localhost.localdomain>
2018-01-31 21:55:24 +0000

    Fix: ilmbase build files

--- ilmbase/2.2.0/package.py
+++ ilmbase/2.2.0/package.py
@@ -2,18 +2,16 @@
 
 name = 'ilmbase'
 
-version = '2.2.0'
+version = '2.2.1'
 
-variants = [['platform-linux', 'arch-x86_64']]
+variants = [
+    ['platform-linux', 'arch-x86_64'],
+]
+
 
 def commands():
     appendenv('ILMBASE_ROOT', '{root}')
 
     if building:
-        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
-        env.ILMBASE_INCLUDE_DIR = "{root}/include"
-
-        # static libs only, hence build-time only
         env.LD_LIBRARY_PATH.append("{root}/lib")
-
-
+        env.ILMBASE_INCLUDE_DIR = "{root}/include"
