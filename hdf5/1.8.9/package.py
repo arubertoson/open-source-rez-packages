af03b1332b195bf2ef4df10737fc5b872c5830f0
macke <macke@localhost.localdomain>
2018-01-31 20:17:00 +0000

    Add: hdf5 package build

--- hdf5/1.8.9/package.py
+++ hdf5/1.8.9/package.py
@@ -4,16 +4,14 @@ name = 'hdf5'
 
 version = '1.8.9'
 
-variants = [['platform-linux', 'arch-x86_64']]
+variants = [
+    ['platform-linux', 'arch-x86_64'],
+]
+
 
 def commands():
     appendenv('HDF5_ROOT', '{root}')
 
     if building:
-        env.PKG_CONFIG_PATH.append("{root}/lib/pkgconfig")
         env.HDF5_INCLUDE_DIR = "{root}/include"
-
-        # static libs only, hence build-time only
         env.LD_LIBRARY_PATH.append("{root}/lib")
-
-
